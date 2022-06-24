284be148f0d188096208dd05dcb35163cfe49a06
SinJ <johankim27@gmail.com>
2022-06-24 23:45:24 +0900

    0624

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { Dialog, Button } from "@mui/material";
 import { MainPageStateContext } from "../../context/stateContext";
 import CopyToClipboard from "react-copy-to-clipboard";
 
 const Modal = () => {
-  const { modalTxt, popModal, setPopModal, setPopToast } =
+  const { popModal, setPopModal, setPopToast } =
     useContext(MainPageStateContext);
   const handleClick = (who) => {
     setPopToast(`계좌가 복사되었습니다.\n(예금주: ${who})`);
@@ -17,18 +17,22 @@ const Modal = () => {
       {popModal === "groom" ? (
         <StAccountCont>
           <div className="account-cont">
-            <div className="font">
-              예금주: <b>김신재</b>
+            <div className="font-cont">
+              <div className="font">KB국민</div>
+              <div className="font">{`${process.env.REACT_APP_GROOM}`}</div>
+              <div className="font">
+                예금주: <b>김신재</b>
+              </div>
             </div>
             <div className="btn-cont">
               <CopyToClipboard
-                text={`${process.env.REACT_APP_GROOM}`}
+                text={`KB국민 ${process.env.REACT_APP_GROOM}`}
                 onCopy={() => {
                   handleClick("김신재");
                 }}
               >
                 <Button className="donation" variant="contained">
-                  계좌 복사하기
+                  계좌 복사
                 </Button>
               </CopyToClipboard>
             </div>
@@ -36,36 +40,45 @@ const Modal = () => {
         </StAccountCont>
       ) : popModal === "bride" ? (
         <StAccountCont>
-          <div className="account-cont">
-            <div className="font">
-              예금주: <b>이남숙</b>
+          <div className="account-cont first">
+            <div className="font-cont">
+              <div className="font">농협</div>
+              <div className="font">{`${process.env.REACT_APP_BRIDE0}`}</div>
+              <div className="font">
+                예금주: <b>이남숙</b>
+              </div>
             </div>
             <div className="btn-cont">
               <CopyToClipboard
-                text={`${process.env.REACT_APP_BRIDE0}`}
+                text={`농협 ${process.env.REACT_APP_BRIDE0}`}
                 onCopy={() => {
                   handleClick("이남숙");
                 }}
               >
                 <Button className="donation" variant="contained">
-                  계좌 복사하기
+                  계좌 복사
                 </Button>
               </CopyToClipboard>
             </div>
           </div>
+          <div className="line"></div>
           <div className="account-cont last">
-            <div className="font">
-              예금주: <b>이세진</b>
+            <div className="font-cont">
+              <div className="font">KB국민</div>
+              <div className="font">{`${process.env.REACT_APP_BRIDE1}`}</div>
+              <div className="font">
+                예금주: <b>이세진</b>
+              </div>
             </div>
             <div className="btn-cont">
               <CopyToClipboard
-                text={`${process.env.REACT_APP_BRIDE1}`}
+                text={`KB국민 ${process.env.REACT_APP_BRIDE1}`}
                 onCopy={() => {
                   handleClick("이세진");
                 }}
               >
                 <Button className="donation" variant="contained">
-                  계좌 복사하기
+                  계좌 복사
                 </Button>
               </CopyToClipboard>
             </div>
@@ -87,9 +100,20 @@ const StAccountCont = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    &.last {
-      margin-top: ${({ theme }) => theme.resHpx(48, theme)};
+    &.first {
+      margin-bottom: ${({ theme }) => theme.resHpx(24, theme)};
     }
+    &.last {
+      margin-top: ${({ theme }) => theme.resHpx(24, theme)};
+    }
+  }
+
+  .font-cont {
+    line-height: 1.5;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: start;
   }
 
   .btn-cont {
@@ -101,5 +125,11 @@ const StAccountCont = styled.div`
     .donation {
       background: black;
     }
+  }
+
+  .line {
+    width: 100%;
+    height: 1px;
+    background: #e2e2e2;
   }
 `;
